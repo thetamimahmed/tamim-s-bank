93c60f8548960aee633f785a1cf259b2ae91a327
thetamimahmed <wwwtamim000@gmail.com>
2022-03-27 23:45:04 +0600

    reduce js code by adding function and added condition on withdraw and deposit input

--- js/banking.js
+++ js/banking.js
@@ -1,41 +1,63 @@
-//deposite money after clicking submit deposite button
-document.getElementById('deposite-btn').addEventListener('click', function(){
-    //get deposite input money
-    const depositeInput = document.getElementById('deposite-input');
-    //get deposite total money
-    const depositeCurrent = document.getElementById('deposite-total');
+//input value get function
+function getInputValue(inputId){
+    //get input money
+    const inputField = document.getElementById(inputId);
     //convert string to number
-    const depositeAmount = parseFloat(depositeInput.value);
-    const depositeTotal = parseFloat(depositeCurrent.innerText)
-    //add inputed deposit money
-    const depositeMoney = depositeTotal + depositeAmount;
-    depositeCurrent.innerText = depositeMoney;
-    //add deposit money in balance
+    const inputAmount = parseFloat(inputField.value);
+    //clear input
+    inputField.value = '';
+    return inputAmount;
+}
+//function of current balance
+function getCurrentBalance(){
     const balanceCurrent = document.getElementById('balance-total');
     const balanceTotal = parseFloat(balanceCurrent.innerText);
-    const balanceSumDeposite = balanceTotal + depositeAmount;
-    balanceCurrent.innerText = balanceSumDeposite;
-    //clear deposite input
-    depositeInput.value = '';
+    return balanceTotal;
+}
+//function of update deposite and withdraw balance
+function updateTotalField(totalFieldId, amount){
+    //get total money
+    const totalElement = document.getElementById(totalFieldId);
+    const currentTotal = parseFloat(totalElement.innerText);
+    //add inputed deposit money
+    const totalMoney = currentTotal + amount;
+    totalElement.innerText = totalMoney;
+}
+//function of update main balance
+function updateBalance(amount, isAddition){
+    //add money in balance
+    const balanceCurrent = document.getElementById('balance-total');
+    const balanceTotal = getCurrentBalance();
+    if(isAddition == true){
+        const balanceSum = balanceTotal + amount;
+        balanceCurrent.innerText = balanceSum;
+    }else{
+        const balanceMinus = balanceTotal - amount;
+        balanceCurrent.innerText = balanceMinus;
+    }
+}
+
+
+
+//deposite money after clicking submit deposite button
+document.getElementById('deposite-btn').addEventListener('click', function(){
+    const depositeAmount = getInputValue('deposite-input');
+    
+    if(depositeAmount > 0){
+        updateTotalField('deposite-total', depositeAmount);
+        updateBalance(depositeAmount, true);
+    }
+    
+   
 })
 
 //withdraw money after clicking submit withdraw button
 document.getElementById('withdraw-btn').addEventListener('click', function(){
-    //get withdraw input money
-    const withdrawInput = document.getElementById('withdraw-input');
-    //get withdraw total money
-    const withdrawCurrent = document.getElementById('withdraw-total');
-    //conver string to number
-    const withdrawAmout = parseFloat(withdrawInput.value);
-    const withdrawTotal = parseFloat(withdrawCurrent.innerText);
-    // add input money to withdraw
-    const withdrawMoney = withdrawTotal + withdrawAmout;
-    withdrawCurrent.innerText = withdrawMoney;
-    //minus withdraw money from balance
-    const balanceCurrent = document.getElementById('balance-total');
-    const balanceTotal = parseFloat(balanceCurrent.innerText);
-    const balanceMinusWithdraw = balanceTotal - withdrawAmout;
-    balanceCurrent.innerText = balanceMinusWithdraw;
-    //clear withdraw input
-    withdrawInput.value ='';
+    const withdrawAmount = getInputValue('withdraw-input');
+
+    const currentBalance = getCurrentBalance();
+    if(withdrawAmount > 0 && withdrawAmount < currentBalance){
+        updateTotalField('withdraw-total', withdrawAmount);
+        updateBalance(withdrawAmount, false);
+    }
 })
--- js/login.js
+++ js/login.js
@@ -1,10 +1,14 @@
+function inputField(inputId){
+    const inputField = document.getElementById(inputId);
+    const inputValue = inputField.value;
+    return inputValue;
+}
+
 document.getElementById('login-btn').addEventListener('click', function(){
     //get user email
-    const emailField = document.getElementById('email-field');
-    const emailInput = emailField.value;
+    const emailInput = inputField('email-field');
     //get user password
-    const passwordField = document.getElementById('password-field');
-    const passwordInput = passwordField.value;
+    const passwordInput = inputField('password-field');
     //condition to enter banking page
     if(emailInput == 'tamim@bank.com' && passwordInput == 'secret'){
         window.location.href = 'banking.html';
